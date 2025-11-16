5f0b0ee11074ed8c517c57ec6273eb7501c5fbc5
lariokabayashi <l247220@dac.unicamp.br>
2025-11-16 13:07:11 +0100

    making UI better

--- MotionApp/Application/AppConstants.swift
+++ MotionApp/Application/AppConstants.swift
@@ -6,10 +6,12 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct AppConstants{
     let backgroundTaskIdentifier = "com.LarissaOkabayashi.MoveCollector.task.process"
     let updateInterval = 1.0/50.0
     let saveThreshold = 500
     let totalTime = .max as Int64
+    let backgroundColor = Color(red: 242/255, green: 242/255, blue: 247/255)
 }
